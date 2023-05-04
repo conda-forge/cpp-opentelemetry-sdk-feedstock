5ac8225018d76baf3d94f4ffc1142d7ac32a3b63
H. Vetinari <h.vetinari@gmx.com>
2023-05-04 16:04:57 +1100

    ingest cpp-opentelemetry-api & rename it to -headers

--- recipe/install.bat
+++ recipe/install.bat
@@ -4,3 +4,11 @@ cd build-cpp
 
 cmake --install .
 if %ERRORLEVEL% neq 0 exit 1
+
+:: for libopentelemetry-cpp-headers, only package the headers
+:: (i.e. remove everything else that's been installed into PREFIX)
+if [%PKG_NAME%] == [libopentelemetry-cpp-headers] (
+    del %LIBRARY_BIN%\opentelemetry*
+    del %LIBRARY_LIB%\opentelemetry*
+    rmdir /s /q %LIBRARY_LIB%\cmake\opentelemetry-cpp
+)
--- recipe/install.sh
+++ recipe/install.sh
@@ -4,3 +4,10 @@ set -ex
 cd build-cpp
 
 cmake --install .
+
+# for libopentelemetry-cpp-headers, only package the headers
+# (i.e. remove everything else that's been installed into PREFIX)
+if [[ "$PKG_NAME" == "libopentelemetry-cpp-headers" ]]; then
+    rm $PREFIX/lib/libopentelemetry*
+    rm -rf $PREFIX/lib/cmake/opentelemetry-cpp
+fi
