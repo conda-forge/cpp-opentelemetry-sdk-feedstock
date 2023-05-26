ade23f75d574a5a6b7f0fa1dfb0a7aadc94d1209
H. Vetinari <h.vetinari@gmx.com>
2023-05-27 02:35:01 +1100

    set -D_LIBCPP_DISABLE_AVAILABILITY

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,6 +15,11 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
     PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc
 fi
 
+if [[ "${target_platform}" == osx-* ]]; then
+  # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+  CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake -GNinja \
     ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
