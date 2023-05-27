18444d16c12077224d9e545891530d4dc350b21c
H. Vetinari <h.vetinari@gmx.com>
2023-05-27 13:21:39 +1100

    set PROTOBUF_USE_DLLS on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,6 +18,9 @@ fi
 if [[ "${target_platform}" == osx-* ]]; then
   # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+  # protobuf switches definitions based on presence of this symbol, for details see
+  # https://github.com/protocolbuffers/protobuf/issues/12746#issuecomment-1546736962
+  CXXFLAGS="${CXXFLAGS} -DPROTOBUF_USE_DLLS"
 fi
 
 cmake -GNinja \
